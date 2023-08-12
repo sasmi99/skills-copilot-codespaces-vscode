1f06fad834cab35bbc0537f97935df14b70eeadf
Sasmi Shehana <98647155+sasmi99@users.noreply.github.com>
2023-08-12 12:00:10 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,14 @@
+// Create web server
+
+// Import module
+const express = require('express');
+const router = express.Router();
+const commentController = require('../controllers/commentController'); 
+
+// Handle request
+router.get('/', commentController.comment_list);
+router.get('/create', commentController.comment_create_get);
+router.post('/create', commentController.comment_create_post);
+router.get('/:id/delete', commentController.comment_create_delete);
+
+
